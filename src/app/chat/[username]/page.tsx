8df2e874fd12c94f8ed9a943071d7577d345c2a0
LongMingWei <longmw2001@gmail.com>
2023-12-06 22:12:52 +0800

    A

--- src/app/chat/[username]/page.tsx
+++ src/app/chat/[username]/page.tsx
@@ -70,7 +70,7 @@ export default function Chat({ params }: { params: { username: string } }) {
 
     fetchData();
     fetchContactData();
-  }, [contact, allContacts]);
+  }, [username, contact, allContacts]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -161,7 +161,7 @@ export default function Chat({ params }: { params: { username: string } }) {
 
 <div className="flex-grow max-h-screen">
 {allContacts.map((contacter, index) => (
-  <div className='flex'>
+  <div key={index} className='flex'>
   <button key={index} onClick={() => setContact(contacter.contactName)}><Contact key={index} latestMessage={contacter.latestMessage} contact={contacter.contactName} /></button>
   <button className='ml-5 mt-auto mb-auto' onClick={() => {deleteContact(username, contacter.contactName)}}><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0,0,256,256">
 <g fill="#ffffff" fill-rule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter"><g transform="scale(8.53333,8.53333)"><path d="M13,3c-0.26757,-0.00363 -0.52543,0.10012 -0.71593,0.28805c-0.1905,0.18793 -0.29774,0.44436 -0.29774,0.71195h-5.98633c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h18c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587h-5.98633c0,-0.26759 -0.10724,-0.52403 -0.29774,-0.71195c-0.1905,-0.18793 -0.44836,-0.29168 -0.71593,-0.28805zM6,8v16c0,1.105 0.895,2 2,2h14c1.105,0 2,-0.895 2,-2v-16z"></path></g></g>
